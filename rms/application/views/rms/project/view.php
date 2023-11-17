39027f6462d7267c0d4d08259802eba7742676c2
andinovid <andi.novid@gmail.com>
2023-11-17 10:39:58 +0700

    update rekap

--- rms/application/views/rms/project/view.php
+++ rms/application/views/rms/project/view.php
@@ -216,7 +216,7 @@
                   <a href="<?php echo base_url(); ?>assets/rms/documents/do/<?php echo $project->file_do; ?>" class="link-black text-sm mr-3" target="_blank"><i class="fas fa-file mr-1"></i> File DO</a>
                   <!--a href="#" class="text-sm btn btn-danger btn-sm text-sm mr-1"><i class="fas fa-print mr-1"></i> Cetak Invoice</a-->
                   <a href="<?php echo base_url(); ?>project/kwitansi/<?php echo $project->id_project; ?>" class="text-sm btn btn-success btn-sm text-sm"><i class="fas fa-print mr-1"></i> Kwitansi</a>
-                  <a href="<?php echo base_url(); ?>rms/download_replas/<?php echo $project->id_project; ?>" class="text-sm btn btn-success btn-sm text-sm"><i class="fas fa-download mr-1"></i> Download data replas</a>
+                  <a href="<?php echo base_url(); ?>rms/download_replas/<?php echo $project->id_project; ?>" class="text-sm btn btn-success btn-sm text-sm"><i class="fas fa-download mr-1"></i> Download data rekap</a>
                 </div>
               </div>
             </div>
