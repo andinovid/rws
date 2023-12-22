3b6b710b48d36bba9c6870ea4eb056a291bc62b9
andinovid <andi.novid@gmail.com>
2023-12-22 15:37:47 +0700

    update website

--- index.php
+++ index.php
@@ -1,1 +1,129 @@
-Under development
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>CV Raja Wali Sampit</title>
+  <meta name="description" content="CV Raja Wali Sampit adalah perusahaan penyedia jasa angkutan / transporter yang berbasis di Sampit, Kalimantan Tengah.">
+  <meta name="robots" content="noindex,nofollow">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+  <section class="page-1">
+    <article class="article text-center">
+      <img src="images/logo_depan.png" width="500" class="" />
+      <h2>GENERAL CONTRACTOR - TRADING - TRANSPORTER</h2>
+    </article>
+    <div class="parallax"></div>
+  </section>
+  <section class="page-2">
+    <article>
+      <div class="container">
+        <h1 class="text-center">
+          TENTANG KAMI
+        </h1>
+        <p class="text-center text-white mt-5">
+          CV Raja Wali Sampit adalah perusahaan penyedia jasa angkutan / transporter yang berbasis di Sampit, Kalimantan Tengah. Perusahaan kami bergerak di bidang
+          pengangkutan berbagai komoditas menggunakan armada dump truck yang modern dan efisien. Dengan dedikasi kami
+          untuk memberikan layanan terbaik, kami telah membangun reputasi sebagai mitra yang andal dalam memenuhi
+          kebutuhan logistik dan transportasi pelanggan kami. Kami berkomitmen untuk memberikan solusi logistik yang
+          inovatif dan handal kepada setiap pelanggan kami. Dengan fokus pada keamanan, efisiensi, dan kepuasan pelanggan,
+          CV Raja Wali Sampit menjadi mitra pilihan untuk kebutuhan angkutan berbagai komoditas.
+        </p>
+      </div>
+    </article>
+  </section>
+  <section class="page-2">
+    <article class="article text-center">
+      <div class="container">
+        <h1 class="text-center">
+          KOMODITAS ANGKUT
+        </h1>
+        <p class="text-center text-white mt-5"> Kami memiliki armada dump truck yang terdiri dari kendaraan-kendaraan
+          terbaru dengan kapasitas angkut yang besar, memastikan kami dapat menangani volume barang yang signifikan
+          dengan efisiensi tinggi. Kami mampu mengangkut berbagai jenis komoditas sesuai dengan kebutuhan pelanggan
+          kami.</p>
+        <div class="row mt-5">
+          <div class="col">
+            <img src="images/pupuk.png" width="80px" />
+            <h3 class="text-white mt-3">Pupuk</h3>
+          </div>
+          <div class="col">
+            <img src="images/kernel.png" width="80px" />
+            <h3 class="text-white mt-3">Kernel</h3>
+          </div>
+          <div class="col">
+            <img src="images/cangkang.png" width="80px" />
+            <h3 class="text-white mt-3">Cangkang</h3>
+          </div>
+          <div class="col">
+            <img src="images/bungkil.png" width="80px" />
+            <h3 class="text-white mt-3">Bungkil</h3>
+          </div>
+          <div class="col">
+            <img src="images/sawit.png" width="80px" />
+            <h3 class="text-white mt-3">Sawit</h3>
+          </div>
+          <div class="col">
+            <img src="images/bata.png" width="80px" />
+            <h3 class="text-white mt-3">Bata</h3>
+          </div>
+          <div class="col">
+            <img src="images/semen.png" width="80px" />
+            <h3 class="text-white mt-3">Semen</h3>
+          </div>
+          <div class="col">
+            <img src="images/pasir.png" width="80px" />
+            <h3 class="text-white mt-3">Pasir</h3>
+          </div>
+        </div>
+        <div class="row mt-5">
+
+        </div>
+      </div>
+    </article>
+    <div class="parallax"></div>
+  </section>
+  <section class="page-2">
+    <article>
+      <div class="container">
+        <h1>
+          HUBUNGI KAMI
+        </h1>
+        <div class="row mt-5">
+          <div class="col-md-3">
+
+            <img src="images/logo_depan.png" width="100%" class="" />
+          </div>
+          <div class="col-md-8">
+            <ul class="p-0 m-0 mt-5">
+              <li class="d-flex">
+                <img src="images/pin.png" width="30" height="30"/>
+                <p class="text-white ml-3">Jl. Wengga Jaya Agung Jalur 4 No. 378 Sampit, Kalimantan Tengah</p>
+              </li>
+              <li class="d-flex">
+                <img src="images/phone.png" width="30" height="30"/>
+                <p class="text-white ml-3">+62 823-3566-0863</p>
+              </li>
+              <li class="d-flex">
+                <img src="images/mail.png" width="30" height="30"/>
+                <p class="text-white ml-3">info@rajawalisampit.com</p>
+              </li>
+              <li class="d-flex">
+                <img src="images/web.png" width="30" height="30"/>
+                <p class="text-white ml-3">https://rajawalisampit.com/</p>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+      </div>
+    </article>
+  </section>
+
+</body>
+
+</html>
