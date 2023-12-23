e3ec3fdba6bf0c767b99eee0a41bcca1a2d73c3e
andinovid <andi.novid@gmail.com>
2023-12-23 09:12:50 +0700

    update web

--- index.php
+++ index.php
@@ -8,21 +8,43 @@
   <meta name="description" content="CV Raja Wali Sampit adalah perusahaan penyedia jasa angkutan / transporter yang berbasis di Sampit, Kalimantan Tengah.">
   <meta name="robots" content="noindex,nofollow">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
   <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
-  <section class="page-1">
+  <nav class="navbar sticky-top navbar-expand-lg">
+    <div class="container">
+      <button class="navbar-toggler text-white" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <i class="fas fa-bars"></i>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto w-100 justify-content-center">
+          <li class="nav-item active">
+            <a class="nav-link" href="#home">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#about">Tentang Kami</a>
+          <li class="nav-item">
+            <a class="nav-link" href="#komoditas">Komoditas</a>
+          <li class="nav-item">
+            <a class="nav-link" href="#contact">Hubungi Kami</a>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <section class="page-1" id="home">
     <article class="article text-center">
-      <img src="images/logo_depan.png" width="500" class="logo" alt="raja wali sampit" title="Raja Wali Sampit" />
+      <img src="images/logo_depan.png" width="450" class="logo" alt="raja wali sampit" title="Raja Wali Sampit" />
       <h2>GENERAL CONTRACTOR - TRADING - TRANSPORTER</h2>
     </article>
     <div class="parallax"></div>
   </section>
-  <section class="page-2">
+  <section class="page-2" id="about">
     <article>
-      <div class="container">
-        <h1 class="text-center">
+      <div class="container text-center">
+        <img src="images/lightbulb.png" width="60" />
+        <h1 class="text-center mt-3">
           TENTANG KAMI
         </h1>
         <p class="text-center text-white mt-5">
@@ -36,10 +58,11 @@
       </div>
     </article>
   </section>
-  <section class="page-2">
+  <section class="page-2" id="komoditas">
     <article class="article text-center">
       <div class="container">
-        <h1 class="text-center">
+        <img src="images/commodity.png" width="60" />
+        <h1 class="text-center mt-3">
           KOMODITAS ANGKUT
         </h1>
         <p class="text-center text-white mt-5"> Kami memiliki armada dump truck yang terdiri dari kendaraan-kendaraan
@@ -65,7 +88,11 @@
           </div>
           <div class="col">
             <img src="images/sawit.png" width="80px" alt="raja wali sampit" title="Raja Wali Sampit" />
-            <h3 class="text-white mt-3">Sawit</h3>
+            <h3 class="text-white mt-3">Buah Sawit</h3>
+          </div>
+          <div class="col">
+            <img src="images/palm-oil.png" width="80px" alt="raja wali sampit" title="Raja Wali Sampit" />
+            <h3 class="text-white mt-3">CPO</h3>
           </div>
           <div class="col">
             <img src="images/bata.png" width="80px" alt="raja wali sampit" title="Raja Wali Sampit" />
@@ -87,33 +114,33 @@
     </article>
     <div class="parallax"></div>
   </section>
-  <section class="page-2">
+  <section class="page-2" id="contact">
     <article>
-      <div class="container">
-        <h1>
+      <div class="container text-center">
+        <img src="images/location.png" width="60" />
+        <h1 class="text-center mt-3">
           HUBUNGI KAMI
         </h1>
-        <div class="row mt-5">
+        <div class="row mt-5 justify-content-md-center">
           <div class="col-md-3">
-
             <img src="images/logo_depan.png" width="100%" class="logo" alt="raja wali sampit" title="Raja Wali Sampit" />
           </div>
-          <div class="col-md-8">
+          <div class="col-md-7">
             <ul class="p-0 m-0 mt-5">
               <li class="d-flex">
-                <img src="images/pin.png" width="30" height="30"/>
+                <img src="images/pin.png" width="30" height="30" />
                 <p class="text-white ml-3">Jl. Wengga Jaya Agung Jalur 4 No. 378 Sampit, Kalimantan Tengah</p>
               </li>
               <li class="d-flex">
-                <img src="images/phone.png" width="30" height="30"/>
+                <img src="images/phone.png" width="30" height="30" />
                 <p class="text-white ml-3">+62 823-3566-0863</p>
               </li>
               <li class="d-flex">
-                <img src="images/mail.png" width="30" height="30"/>
+                <img src="images/mail.png" width="30" height="30" />
                 <p class="text-white ml-3"><a href="mailto:info@rajawalisampit.com">info@rajawalisampit.com</a></p>
               </li>
               <li class="d-flex">
-                <img src="images/web.png" width="30" height="30"/>
+                <img src="images/web.png" width="30" height="30" />
                 <p class="text-white ml-3"><a href="https://rajawalisampit.com/">https://rajawalisampit.com/</a></p>
               </li>
             </ul>
@@ -125,5 +152,58 @@
   </section>
 
 </body>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+
+
+<script>
+  //Function to mark an item from a section width a css class.
+  function menuOnScroll(mySection, myMenu, myClass) {
+    $(window).scroll(function() {
+      var elScroll = $(window).scrollTop();
+      $(mySection).each(function(i) {
+        if ($(this).offset().top <= elScroll) {
+          $(myMenu).removeClass(myClass);
+          $(myMenu).eq(i).addClass(myClass);
+        }
+      });
+    });
+  }
+  //Call function.
+  //First parameter will be the section that we want to go.
+  //Second will be the item that will change his css.
+  //Third will be the class css to add to the item( Our second parameter) Is IMPORTAT to note that we must to skip the dot of our class.
+  menuOnScroll('section', 'nav ul li a', 'active');
+
+
+
+  //Function to animate the scroll when click on a menu item.
+  //IMPORTANT. This function is only use for animate the scroll, you could skip it if you want.
+  function scrollToAnyPoint(navItem) {
+    var getAttr;
+    $(navItem).click(function(e) {
+      e.preventDefault();
+      getAttr = $(this).attr('href');
+      var toSection = $(getAttr).offset().top;
+      $("html, body").animate({
+        scrollTop: toSection
+      }, 1000)
+    });
+  }
+  //Call Function
+  scrollToAnyPoint('nav ul li a');
+
+  $(document).ready(function() {
+    $(window).scroll(function() {
+      var scroll = $(window).scrollTop();
+      if (scroll > 300) {
+        $(".navbar").css("background", "rgba(0, 0, 0, 0.7)");
+      } else {
+        $(".navbar").css("background", "rgba(0, 0, 0, 0)");
+      }
+    })
+  })
+</script>
 
 </html>
