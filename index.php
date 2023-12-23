41a180619219684bc3b26df4846920f7663da62c
andinovid <andi.novid@gmail.com>
2023-12-23 12:45:06 +0700

    update web

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto w-100 justify-content-center">
           <li class="nav-item active">
-            <a class="nav-link" href="#home">Home <span class="sr-only">(current)</span></a>
+            <a class="nav-link active" href="#home">Home <span class="sr-only">(current)</span></a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="#about">Tentang Kami</a>
